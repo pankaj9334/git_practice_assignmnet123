5c4ea505e154fdac1d0199d7a17cd52fdcd07981
pankaj9334 <pankaj933426@gmail.com>
2022-11-30 13:11:37 +0530

    I have created problems

---
+++ plindrome_problem.js
@@ -0,0 +1,9 @@
+  let str="naman";
+  let bag="";
+  for(let i=str.length-1;i>=0;i--){
+    bag+=str[i];
+  }
+  if(bag!=str){
+  console.log("Palindrom");
+  }else{
+  console.log("Not Palindrom");
